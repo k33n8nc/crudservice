78a50c53ac209f5b81226dca943e6221916dae20
Robbert Stapel <robbert.stapel@live.nl>
2023-05-01 18:08:14 +0200

    Code cleanup.

--- src/main/java/nl/robbertstapel/crudservice/CrudserviceApplication.java
+++ src/main/java/nl/robbertstapel/crudservice/CrudserviceApplication.java
@@ -6,15 +6,8 @@ import java.time.LocalDate;
 
 @SpringBootApplication
 public class CrudserviceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CrudserviceApplication.class, args);
 	}
 
-	Customer cus1 = new Customer(
-			"Robbert",
-			"Stapel",
-			LocalDate.of(1989, 5, 17),
-			21,
-			"Kerkstraat 1");
 }
