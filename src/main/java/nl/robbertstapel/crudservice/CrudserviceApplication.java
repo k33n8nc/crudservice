9fcab1eaf2e0e55da2abe449b8ede472800b8608
Robbert Stapel <robbert.stapel@live.nl>
2023-05-01 13:13:20 +0200

    Cleanup code and test object to use with LocalDate. It works!

--- src/main/java/nl/robbertstapel/crudservice/CrudserviceApplication.java
+++ src/main/java/nl/robbertstapel/crudservice/CrudserviceApplication.java
@@ -1,9 +1,8 @@
 package nl.robbertstapel.crudservice;
-
+import nl.robbertstapel.crudservice.customer.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class CrudserviceApplication {
@@ -11,4 +10,11 @@ public class CrudserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CrudserviceApplication.class, args);
 	}
+
+	Customer cus1 = new Customer(
+			"Robbert",
+			"Stapel",
+			LocalDate.of(1989, 5, 17),
+			21,
+			"Kerkstraat 1");
 }
