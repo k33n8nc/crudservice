971e8ea5d7aa0c9b5373a39cadb529d2588b7d2f
Robbert Stapel <robbert.stapel@live.nl>
2023-05-01 13:11:47 +0200

    Replaced Date() from java.util.Date to LocalDate from java.time.LocalDate because of deprication.

--- src/main/java/nl/robbertstapel/crudservice/customer/Customer.java
+++ src/main/java/nl/robbertstapel/crudservice/customer/Customer.java
@@ -1,15 +1,14 @@
 package nl.robbertstapel.crudservice.customer;
-
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Customer {
     private String firstName; // unique in combination with lastName
     private String lastName; // unique in combination with firstName
-    private Date dateOfBirth;
+    private LocalDate dateOfBirth;
     private int age; // calculated from dateOfBirth
     private String address;
 
-    public Customer(String firstName, String lastName, Date dateOfBirth, int age, String address) {
+    public Customer(String firstName, String lastName, LocalDate dateOfBirth, int age, String address) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
