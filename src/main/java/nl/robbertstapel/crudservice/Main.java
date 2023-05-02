cc4a038635b82283ebc3f7277bf6ec80b929cf50
Robbert Stapel <robbert.stapel@live.nl>
2023-05-03 00:33:19 +0200

    Restructure application

--- src/main/java/nl/robbertstapel/crudservice/Main.java
+++ src/main/java/nl/robbertstapel/crudservice/Main.java
@@ -1,13 +1,12 @@
 package nl.robbertstapel.crudservice;
-import nl.robbertstapel.crudservice.customer.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.time.LocalDate;
 
 @SpringBootApplication
-public class CrudserviceApplication {
+public class Main {
+
 	public static void main(String[] args) {
-		SpringApplication.run(CrudserviceApplication.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 
 }
